37abaabecb9212f05a385cf3ce2ebc8989b395b1
Dylan <djaycoetzee@live.com>
2019-11-25 22:18:03 +0200

    Completed login and logout function for alamofire rest request.

---
+++ FoodTaskerMobile/Manager/APIManager.swift
@@ -0,0 +1,75 @@
+//
+//  APIManager.swift
+//  FoodTaskerMobile
+//
+//  Created by Dylan on 2019/11/25.
+//  Copyright © 2019 Dylan. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import FBSDKLoginKit
+
+class APIManager {
+    static let shared = APIManager()
+    let baseURL = NSURL(string: BASE_URL)
+    
+    var accessToken = ""
+    var refreshToken = ""
+    var expired = Date()
+    
+    //Apli to login user
+    func login(userType: String, completionHandler: @escaping (NSError?) -> Void) {
+        let path = "api/social/convert-token"
+        let url = baseURL!.appendingPathComponent(path)
+        let params: [String: Any] = [
+            "grant_type": "convert-token",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "backend": "facebook",
+            "token": AccessToken.current!.tokenString,
+            "user_type": userType
+        ]
+        
+        AF.request(url!, method: .post, parameters: params, encoding: URLEncoding(), headers: nil).responseJSON { (response) in
+            switch response.result {
+                
+                case .success(let value):
+                    let jsonData = JSON(value)
+                    self.accessToken = jsonData["access_token"].string!
+                    self.refreshToken = jsonData["redresh_token"].string!
+                    self.expired = Date().addingTimeInterval(TimeInterval(jsonData["expires_in"].int!))
+                    
+                    completionHandler(nil)
+                    
+                case .failure(let error):
+                    completionHandler(error as NSError?)
+                    break
+            }
+        }
+    }
+    
+    //Api to logout user
+    func logout(completionHandler: @escaping (NSError?) -> Void) {
+        let path = "api/social/revoke-token"
+        let url = baseURL!.appendingPathComponent(path)
+        let params: [String: Any] = [
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "token": self.accessToken,
+        ]
+        
+        AF.request(url!, method: .post, parameters: params, encoding: URLEncoding(), headers: nil).responseString { (response) in
+            switch response.result {
+                case .success:
+                    completionHandler(nil)
+                    break
+                    
+                case .failure(let error):
+                    completionHandler(error as NSError?)
+                    break
+            }
+        }
+    }
+}
---
+++ FoodTaskerMobile/Manager/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  FoodTaskerMobile
+//
+//  Created by Dylan on 2019/11/25.
+//  Copyright © 2019 Dylan. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL: String = "http://localhost:8000"
+let CLIENT_ID: String = "XO8BahFNWaCZFOACwPpyhrceGYYG6xs1dgaPjnGq"
+let CLIENT_SECRET: String = "EHb4IydQ5y3aSm6LZ7p0DFVMixjY9l5YjiHwghs8Hs1Km5XKKYgPY7S7Zce4qc9tlDVJsf326N0aJaycDSWlvzQ74YnC3N8LIqpLdo2tH33IKssOO3v7nZZIaKqTS4aP"
+
+let USERTYPE_CUSTOMER = "customer"
+let USERTYPE_DRIVER = "driver"
+
