9804309071d7ea53a4ce96242cd379d2e4de97ea
Dylan <djaycoetzee@live.com>
2020-01-21 23:39:41 +0200

    Location not working on view tray

--- FoodTaskerMobile/Controller/MealDetailsViewController.swift
+++ FoodTaskerMobile/Controller/MealDetailsViewController.swift
@@ -49,24 +49,51 @@ class MealDetailsViewController: UIViewController {
         
         let trayItem = TrayItem(meal: self.meal!, qty: self.qty)
         guard let trayRestaurant = Tray.currentTray.restaurant, let currentRestaurant = self.restaurant else {
+            //If requirements are not met
             Tray.currentTray.restaurant = self.restaurant
             Tray.currentTray.items.append(trayItem)
             return
         }
         
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel)
+        
+        //If ordering from the same restauarant
         if trayRestaurant.id == currentRestaurant.id {
-            let inTray = Tray.currentTray.items.index(where: {(item) -> Bool in
+            let inTray = Tray.currentTray.items.firstIndex(where: {(item) -> Bool in
                 return item.meal.id! == trayItem.meal.id!
             })
             
             if let index = inTray {
                 let alertView = UIAlertController (title: "Add more?", message: "Your tray already has this meal. Do you want to add more?", preferredStyle: .alert)
-                //let okAction = IUAlertAction(title: "A")
+                
+                let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (action) -> Void in
+                    Tray.currentTray.items[index].qty += self.qty
+                }
+                
+                alertView.addAction(okAction)
+                alertView.addAction(cancelAction)
+                
+                self.present(alertView,animated: true, completion: nil)
             }
-        } else {
+            else {
+                Tray.currentTray.items.append(trayItem)
+            }
             
+        } else {
+            //if ordering from a different restauarant
+            let alertView = UIAlertController (title: "Start new tray?", message: "You're ordering a meal from another restaurant. Would you like to clear the current tray?", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "New Tray", style: UIAlertAction.Style.default) { (action) -> Void in
+                Tray.currentTray.items = []
+                Tray.currentTray.items.append(trayItem)
+                Tray.currentTray.restaurant = self.restaurant
+            }
+            
+            alertView.addAction(okAction)
+            alertView.addAction(cancelAction)
+            
+            self.present(alertView,animated: true, completion: nil)
         }
-        
     }
     
     @IBAction func addQty(_ sender: UIButton) {
--- FoodTaskerMobile/Controller/TrayViewController.swift
+++ FoodTaskerMobile/Controller/TrayViewController.swift
@@ -7,10 +7,22 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class TrayViewController: UIViewController {
 
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    @IBOutlet weak var tbvMeals: UITableView!
+    @IBOutlet weak var viewTotal: UIView!
+    @IBOutlet weak var viewAddress: UIView!
+    @IBOutlet weak var viewMap: UIView!
+    @IBOutlet weak var lbTotal: UILabel!
+    @IBOutlet weak var tbAddress: UITextField!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var bAddPayment: UIButton!
+    
+    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +32,53 @@ class TrayViewController: UIViewController {
             menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        if Tray.currentTray.items.count == 0 {
+            let lbEmptyTray = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 40))
+            lbEmptyTray.center = self.view.center
+            lbEmptyTray.textAlignment = NSTextAlignment.center
+            lbEmptyTray.text = "Your Tray is empty. Please select meal."
+            
+            self.view.addSubview(lbEmptyTray)
+            
+        } else {
+            //Show all of the hidden items
+            self.tbvMeals.isHidden = false
+            self.viewTotal.isHidden = false
+            self.viewAddress.isHidden = false
+            self.viewMap.isHidden = false
+            self.bAddPayment.isHidden = false
+            
+            loadMeals()
+        }
+        
+        //Show current user location
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+            
+            self.map.showsUserLocation = true
+        }
+    }
+    
+    func loadMeals() {
+        self.tbvMeals.reloadData()
+        self.lbTotal.text = "$\(Tray.currentTray.getTotal())"
+    }
+}
+
+extension TrayViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.map.setRegion(region, animated: true)
     }
 }
 
